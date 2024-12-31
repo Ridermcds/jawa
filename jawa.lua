5a1591ba802bf5fcc860b45c51d14102a1185d90
Ridermcds <ridermcds@gmail.com>
2024-12-31 14:16:55 +0700

    Update jawa.lua

--- jawa.lua
+++ jawa.lua
@@ -26,17 +26,17 @@ if placeId == 72521583116265 then
     wait(120)
 
     local npcs = {
-        {location = workspace.Character.DA0ZA.LowerTorso, prompt = workspace.Character.DA0ZA.ProximityPrompt, delay = 4},
-        {location = workspace.Character.MisterOtnas.LowerTorso, prompt = workspace.Character.MisterOtnas.ProximityPrompt, delay = 4},
-        {location = workspace.Character.Pengemudiindonesiaa.LowerTorso, prompt = workspace.Character.Pengemudiindonesiaa.ProximityPrompt, delay = 4},
-        {location = workspace.Character.Tippenzs.LowerTorso, prompt = workspace.Character.Tippenzs.ProximityPrompt, delay = 4},
-        {location = workspace.Character.Used_Legos.LowerTorso, prompt = workspace.Character.Used_Legos.ProximityPrompt, delay = 4},
-        {location = workspace.Character.denannr.LowerTorso, prompt = workspace.Character.denannr.ProximityPrompt, delay = 4},
-        {location = workspace.Character.rew_267.LowerTorso, prompt = workspace.Character.rew_267.ProximityPrompt, delay = 4},
-        {location = workspace.Character.rungkadkacaw.LowerTorso, prompt = workspace.Character.rungkadkacaw.ProximityPrompt, delay = 4},
-        {location = workspace.Character.the_smash9.LowerTorso, prompt = workspace.Character.the_smash9.ProximityPrompt, delay = 4},
-        {location = workspace.Character.wheresjulmans.LowerTorso, prompt = workspace.Character.wheresjulmans.ProximityPrompt, delay = 4},
-        {location = workspace.Character.yahyasenuadjie.LowerTorso, prompt = workspace.Character.yahyasenuadjie.ProximityPrompt, delay = 4},
+        {location = workspace.Character.DA0ZA.LowerTorso, prompt = workspace.Character.DA0ZA.ProximityPrompt, delay = 12},
+        {location = workspace.Character.MisterOtnas.LowerTorso, prompt = workspace.Character.MisterOtnas.ProximityPrompt, delay = 12},
+        {location = workspace.Character.Pengemudiindonesiaa.LowerTorso, prompt = workspace.Character.Pengemudiindonesiaa.ProximityPrompt, delay = 12},
+        {location = workspace.Character.Tippenzs.LowerTorso, prompt = workspace.Character.Tippenzs.ProximityPrompt, delay = 12},
+        {location = workspace.Character.Used_Legos.LowerTorso, prompt = workspace.Character.Used_Legos.ProximityPrompt, delay = 12},
+        {location = workspace.Character.denannr.LowerTorso, prompt = workspace.Character.denannr.ProximityPrompt, delay = 6},
+        {location = workspace.Character.rew_267.LowerTorso, prompt = workspace.Character.rew_267.ProximityPrompt, delay = 6},
+        {location = workspace.Character.rungkadkacaw.LowerTorso, prompt = workspace.Character.rungkadkacaw.ProximityPrompt, delay = 6},
+        {location = workspace.Character.the_smash9.LowerTorso, prompt = workspace.Character.the_smash9.ProximityPrompt, delay = 6},
+        {location = workspace.Character.wheresjulmans.LowerTorso, prompt = workspace.Character.wheresjulmans.ProximityPrompt, delay = 6},
+        {location = workspace.Character.yahyasenuadjie.LowerTorso, prompt = workspace.Character.yahyasenuadjie.ProximityPrompt, delay = 6},
     }
 
     for _, npc in ipairs(npcs) do
@@ -47,8 +47,8 @@ if placeId == 72521583116265 then
     end
 
     local itemsAndAnimals = {
-        {location = workspace.Character.Carrot.Carrot, prompt = workspace.Character.Carrot.ProximityPrompt, delay = 6},
-        {location = workspace.Character.Rabbit.mesh_id31, prompt = workspace.Character.Rabbit.ProximityPrompt, delay = 6},
+        {location = workspace.Character.Carrot.Carrot, prompt = workspace.Character.Carrot.ProximityPrompt, delay = 10},
+        {location = workspace.Character.Rabbit.mesh_id31, prompt = workspace.Character.Rabbit.ProximityPrompt, delay = 10},
         {location = workspace.Character.Racoon["Meshes/RaccoonSized"], prompt = workspace.Character.Racoon.ProximityPrompt , delay = 120},
         {location = workspace.Character.Gorilla.MeshPart, prompt = workspace.Character.Gorilla.ProximityPrompt, delay = 10},
     }
